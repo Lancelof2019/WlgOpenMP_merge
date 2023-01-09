145035cae2579e0ed1059c103ef8de0219155605
Lance <53155359+Lancelof2019@users.noreply.github.com>
2023-01-09 11:42:24 +0100

    Update watershed.cpp
    remove comments

--- functionspace/watershed.cpp
+++ functionspace/watershed.cpp
@@ -17,12 +17,6 @@ struct Pixel {
 
     Pixel(int val, int x, int y) : val(val), x(x), y(y) {}
 
-   /*    friend ostream &operator<<(ostream &os, const Pixel &pixel) {
-        os << "val: " << pixel.val << " x: " << pixel.x << " y: " << pixel.y;
-        return os;
-
-    }
-    */
 };
 
 
@@ -43,16 +37,7 @@ int pixelval;
 int pixelnum;
 double ndist;
 nngNode(int x, int y, int pixelval,int pixelnum,double ndist) : x(x), y(y), pixelval(pixelval),pixelnum(pixelnum),ndist(ndist) {}
-/*nngNode(int x,int y,int pixelnngVal,int pixelnngNum,double nngDist){
 
- this->cordx=x;
- this->cordy=y;
- this->pixelnngVal=pixelnngVal;
- this->pixelnngNum=pixelnngNum;
- this->nngDist=nngDist;
-
-}
-*/
 };
 
 struct cmp2{
@@ -80,10 +65,6 @@ void vistNode(vector<nngNode>&nngvec,priority_queue<nngNode,vector<nngNode>,cmp2
 */
 
 Mat WatershedAlg::watershed(Array2D<int> &image, Array2D<int>& markers,Mat &duplImage,int &rows,int &cols,Array2D<bool> &inprioq,Array2D<int>& markerMap,Array2D<int>&temp,Array2D<int>& nextSet, int &id_num){
-
-
-
-//priority_queue<int*,vector<int*>,Compare>prioq;
 
 
        int msize=0;
@@ -131,12 +112,12 @@ int tempcounter=0;
    }
 
          id_num=id;
-	 /////////////////////////////////////
+
          Array2D<int> idArr(id,id,0);
-//	 Array2D<double> neighDis(id,id,0);
+
          int pixelSum[id]={0};
 	 int pixelNum[id]={0};
-       //  memset(pixelNum,-10,sizeof(pixelNum));
+    
 
          
 
@@ -158,31 +139,6 @@ Vec3b boundColor;
 boundColor[0] = 0;
 boundColor[1] = 0;
 boundColor[2] = 0;
-/*
-//cout<<"OK for now"<<endl;
-int ****idArr=new int***[id];
-for(int i=0;i<id;i++){
-   idArr[i]=new int**[id];
-     for(int j=0;j<cols;j++){
-       idArr[i][j]=new int*[1];
-       for(int m=0;m<1;m++){
-        idArr[i][j][m]=new int[4];
-        for(int n=0;n<4;n++){
-           idArr[i][j][m][0]=i*3+j;
-           idArr[i][j][m][1]=0;//number of pixel in region
-           idArr[i][j][m][2]=0;//sum of pixel value
-           idArr[i][j][m][3]=0;//is there any edge
-
-
-
-        }
-
-       }
-     }
-}
-*/
-
-//int idSum=0;
 
 #pragma omp parallel for reduction(+:pixelSum[:id],pixelNum[:id])
 for(int i=0;i<tempcounter;i++){
@@ -195,14 +151,9 @@ for(int i=0;i<tempcounter;i++){
   while(!prioq.empty()){
     int CrtX=prioq.top().x;
     int CrtY=prioq.top().y;
-	  
-	  
-    // int curpoint[3]={prioq.top()[0],prioq.top()[1],prioq.top()[2]};
-    //int curpointx=temp(i,1);
-    //int curpointx=temp(i,2);
+
     prioq.pop();
-    //int CrtX=curpoint[1];
-    //int CrtY=curpoint[2];
+
     bool canLabel = true;
     int neighboursLabel = 0;
     for(int i = 0; i < 8; i++) {
@@ -258,35 +209,9 @@ for(int i=0;i<tempcounter;i++){
             }
     }//while
 }//for
-/*
-     string filename31="./test31.txt";
-     ofstream fout31(filename31);
 
 
-        for(int i = 0; i < id; i++) {
-          for(int j = 0; j < id; j++) {
 
-          fout31<<idArr(i,j)<<",";
-             
-
-        }
-         fout31<<endl;
-     }
-       fout31.close();
-       */
-
-//cout<<"id 5 value:"<<pixelSum[5]<<endl;
-//cout<<"id 5 num:"<<pixelNum[5]<<endl;
-
-//cout<<"id 34 value:"<<pixelSum[34]<<endl;
-//cout<<"id 34 num:"<<pixelNum[34]<<endl;
-       
-//cout<<"id 89 value:"<<pixelSum[89]<<endl;
-//cout<<"id 89 num:"<<pixelNum[89]<<endl;
-//cout<<"idSum:"<<idSum<<endl;
-//Array2D<double>nng(idSum,idSum,0.0);
-//
-//
 int ragNode=0;
 
 
@@ -302,54 +227,20 @@ for(int i=0;i<id;i++){
     double powerDis=pow((((double)pixelSum[i])/(double)pixelNum[i]-((double)pixelSum[j])/(double)pixelNum[j]),2);
     double mulelement=((double)pixelNum[i])*((double)pixelNum[j]);
     double divelement=(double)(pixelNum[i]+pixelNum[j]);
-    /*
-    cout<<"------------------------------------"<<endl;
-    cout<<"powerDis:"<<powerDis<<endl;
-    cout<<"mulelement:"<<mulelement<<endl;
-    cout<<"divelement:"<<divelement<<endl;
-    cout<<"("<<i<<","<<j<<")"<<endl;
-    cout<<"psum"<<pixelSum[i]<<endl;
-    cout<<"pnum"<<pixelNum[i]<<endl;
-    cout<<"distance of"<<"("<<i<<","<<j<<"):"<<((mulelement*powerDis)/divelement)<<endl;
-    */
     neighDist(i,j)=((mulelement*powerDis)/divelement)+1.0;
     nngNode newnode=nngNode(i,j,pixelSum[i],pixelNum[i],neighDist(i,j));
     nngprioq.push(newnode);
        }
      }
 }
-//cout<<"---------------------nng queue---------------------------"<<endl;
+
 int ngnum=0;
 vector<nngNode>nngvec;
-//nngNode prenode=(nngprioq.top().x,nngprioq.top().y,nngprioq.top().pixelval,nngprioq.top().pixelnum,nngprioq.top().ndist);
 while(!nngprioq.empty()){
 	 
         nngNode prenode=nngNode(nngprioq.top().x,nngprioq.top().y,nngprioq.top().pixelval,nngprioq.top().pixelnum,nngprioq.top().ndist);	    nngprioq.pop();
-	 //cout<<"--------------while priority queue----------------------"<<endl;
-         //cout<<ncounter<<"st Element:"<<endl;
-         //cout<<"distanceValue:"<<prenode.ndist<<endl;
-         //cout<<"x:"<<prenode.x<<endl;
-        // cout<<"y:"<<prenode.y<<endl;
-        // cout<<"sumPixel:"<<prenode.pixelval<<endl;
-        // cout<<"numPixel:"<<prenode.pixelnum<<endl;
-	 /*if(prenode.ndist==nngprioq.top().ndist&&prenode.y==nngprioq.top().x&&prenode.x==nngprioq.top().y){
-            cout<<"lable:"<<prenode.x<<" merge lable:"<<nngprioq.top().x<<" into :"<<"("<<prenode.x<<","<<
-           
-	 }
-       //  ncounter++;
-       //  
-	 */
-         
-	 /*
-	 if(nngvec.size()!=0){
-	      for(int i=0;i<nngvec.size();i++){
-                 if(nngvec.at(i).x==nngprioq.top().x){
-                 nngprioq.pop();
-	      } 
 
-	  }
-	 }
-         */
+
         // ncounter++;	 
 	if(!nngprioq.empty()){
          if(prenode.ndist==nngprioq.top().ndist&&prenode.y==nngprioq.top().x&&prenode.x==nngprioq.top().y){
@@ -374,332 +265,9 @@ while(!nngprioq.empty()){
 	}
 }
 
-//cout<<"----------------------------------"<<endl;
 
+//priority_queue<nngNode,vector<nngNode>,cmp2>nngpq;
 
-
-priority_queue<nngNode,vector<nngNode>,cmp2>nngpq;
-
-/*
-for(int i=0;i<nngvec.size();i++){
-*/
-/*
- * *
-         cout<<"--------------------vector of nng nodes------------------------"<<endl;
-         cout<<"distanceValue:"<<nngvec.at(i).ndist<<endl;
-         cout<<"x:"<<nngvec.at(i).x<<endl;
-         cout<<"y:"<<nngvec.at(i).y<<endl;
-         cout<<"sumPixel:"<<nngvec.at(i).pixelval<<endl;
-         cout<<"numPixel:"<<nngvec.at(i).pixelnum<<endl;
-*
-* */
-
-/*
-if(nngvec.at(i).x==nngvec.at(i+1).y&&nngvec.at(i).y==nngvec.at(i+1).x){
-    nngpq.push(nngvec.at(i));
-    
-
-   } 
-
-
-
-
-}
-
-*/
-cout<<"----------------------------------"<<endl;
-//cout<<"vector size:"<<nngvec.size()<<endl;
-
-
-
-
-
-
-/*
-int ngnum=0;
-nngNode *nngp;
-nngp=(nngNode*)malloc(sizeof(nngNode)*ragNode);
-
-//for(int p=0;p<ragNode;p++){
-
-//nngNode(int x, int y, int pixelval,int pixelnum,double ndist) : x(x), y(y), pixelval(pixelval),pixelnum(pixelnum),ndist(ndist) {}
-
-for(int i=0;i<id;i++){
-     nngNode minimavalue=nngNode(i,0,pixelSum[i],pixelNum[i],neighDist(i,0));
-   for(int j=0;j<id;j++){
-     if(neighDist(i,j)>0&&i!=j){	   
-      if(minimavalue.ndist>neighDist(i,j)){
-        // minimavalue.ndist=neighDist(i,j);
-        // minimavalue.x=i;
-        // minimavalue.y=j;
-	// minimavalue.pixelval=pixelSum[i];
-	// minimavalue.pixelnum=pixelNum[i];
-	 minimavalue=nngNode(i,j,pixelSum[i],pixelNum[i],neighDist(i,j));
-         nngp[ngnum]=nngNode(minimavalue.x,minimavalue.y,pixelSum[minimavalue.x],pixelNum[minimavalue.x],neighDist(minimavalue.x,minimavalue.y));
-*/	 /*	 
-	 nngp[ngnum].ndist=minimavalue.ndist;
-         nngp[ngnum].x=minimavalue.x;
-         nngp[ngnum].y=minimavalue.y;
-         nngp[ngnum].pixelval=minimavalue.pixelval;
-         nngp[ngnum].pixelnum=minimavalue.pixelnum; 
-	 */
-  /*      }
-        if(j==(id-1)){
-         ngnum++;		
-	}	
-       
-       }
-      }
-    */  
-     /*
-        if(j==(id-1)){
-         
-         nngp[ngnum].ndist=minimavalue.ndist;
-         nngp[ngnum].x=minimavalue.x;
-         nngp[ngnum].y=minimavalue.y;
-         nngp[ngnum].pixelval=minimavalue.pixelval;
-         nngp[ngnum].pixelnum=minimavalue.pixelnum; 
-         ngnum++;
-	}
-      }
-      */
-     /*
-      //if(minimavalue.ndist>0){
-         nngp[ngnum].ndist=minimavalue.ndist;
-         nngp[ngnum].x=minimavalue.x;
-         nngp[ngnum].y=minimavalue.y;
-         nngp[ngnum].pixelval=minimavalue.pixelval;
-         nngp[ngnum].pixelnum=minimavalue.pixelnum;
-	 cout<<"-----------nngp----------------"<<endl;
-	 cout<<"distanceValue:"<<nngp[ngnum].ndist<<endl;
-	 cout<<"x:"<<nngp[ngnum].x<<endl;
-	 cout<<"y:"<<nngp[ngnum].y<<endl;
-	 cout<<"sumPixel:"<<nngp[ngnum].pixelval<<endl;
-	 cout<<"numPixel:"<<nngp[ngnum].pixelnum<<endl;
-
-
-	 cout<<"###reverse distanceValue:###"<<neighDist(nngp[ngnum].y,nngp[ngnum].x)<<endl;
-         cout<<"x:"<<nngp[ngnum].y<<endl;
-         cout<<"y:"<<nngp[ngnum].x<<endl;
-         cout<<"sumPixel:"<<pixelSum[nngp[ngnum].y]<<endl;
-         cout<<"numPixel:"<<pixelNum[nngp[ngnum].y]<<endl;
-         ngnum++;
-     // }
-*/
-   //free(minimavalue);
-//}
-//cout<<"----------------------"<<endl;
-//cout<<"ngnum"<<ngnum<<endl;
-/*
-
-for(int i=0;i<ngnum;i++){
-      cout<<"-----------nngp----------------"<<endl;
-      cout<<"distanceValue:"<<nngp[i].ndist<<endl;
-      cout<<"x:"<<nngp[i].x<<endl;
-      cout<<"y:"<<nngp[i].y<<endl;
-      cout<<"sumPixel:"<<nngp[i].pixelval<<endl;
-      cout<<"numPixel:"<<nngp[i].pixelnum<<endl;
-}
-
-
-cout<<"----------------------"<<endl;
-//cout<<"ngnum"<<ngnum<<endl;
-//}
-
-*/
-
-/*#pragma omp parallel for 
-for(int i=0;i<id;i++){
-  for(int j=0;j<id;j++){
-    
-   cout<<neighDist[i][j]<<endl;
-    
-   }
-}
-*/
-/*
-string filename32="./test32.txt";
-ofstream fout32(filename32);
-for(int i = 0; i < id; i++) {
-    for(int j = 0; j < id; j++) {
-       fout32<<neighDist(i,j)<<",";
-       //if(neighDis(i,j)<0){
-       //cout<<"negtive"<<endl;
-      }
-       fout32<<endl;
-  }
-   
-fout32.close();
-*/
- // nngNode nngp[ragNode];
-/*
-double __restrict ***nngGraph=new double**[ragNode];
-for(int i=0;i<ragNode;i++){
-   nngGraph[i]=new double*[1];
-   for(int j=0;j<1;j++){
-    nngGraph[i][j]=new double[5];
-     nngGraph[i][j][0]=0;//cordx
-     nngGraph[i][j][1]=0;//cordy
-     nngGraph[i][j][2]=0;//pixelNum
-     nngGraph[i][j][3]=0;//pixelSum
-     nngGraph[i][j][4]=0;//Dist
-   
-   }
-
-}
-
-
-*/
-/*
-  int cmount=0;
-  nngNode tempnode[id];
-  for(int i=0;i<id;i++){
-   nngNode tempnode;
-   tempnode[i].cordx=0;
-   tempnode[i].cordy=0;
-   tempnode[i].pixelnngVal=0;
-   tempnode[i].pixelnngNum=0;
-   tempnode[i].nngDist=0.0;
-   for(int j=0;j<id;j++){
-    if(neighDis(i,j)>0){
-       if(tempnode[i].nngDist==0){
-         tempnode[i].nngDist=neighDis(i,j);
-
-	 }
-
-        if(tempnode[i].nngDist!=0){
-		if(tempnode[i].nngDist>neighDis(i,j)){
-		   tempnode[i].nngDist=neighDis(i,j);
-		   tempnode[i].cordx=i;
-                   tempnode[i].cordy=j;
-                   tempnode[i].pixelnngVal=pixelSum[i];
-                   tempnode[i].pixelnngNum=pixelNum[i];
-                    
-                  //nngGraph[i][j][0]=i;
-                  //nngGraph[i][j][1]=j;
-                  //nngGraph[i][j][2]=pixelNum[i];
-                  //nngGraph[i][j][3]=pixelSum[i];
-                  //nngGraph[i][j][4]=neighDis(i,j);
-         }
-       }
-    }
-   }
-   if(tempnode[i].nngDist!=0){
-      nngp[cmount].nngDist = tempnode[i].nngDist;
-      nngp[cmount].cordx = tempnode[i].cordx;
-      nngp[cmount].cordy = tempnode[i].cordy;
-      nngp[cmount].pixelnngVal = tempnode[i].pixelnngVal;
-      nngp[cmount].pixelnngNum = tempnode[i].pixelnngNum;
-      cmount++; 
-      cout<<"Distance:"<<nngp[cmount].cordx<<":"<<nngp[cmount].nngDist<<endl;      
-   }
-}
-*/
-/*
-string filename33="./test33.txt";
-ofstream fout33(filename33);
-for(int i = 0; i < ragNode; i++) {
- //   for(int j = 0; j < id; j++) {
-    // fout32<<neighDis(i,j)<<",";
-    // if(neighDis(i,j)<0){
-     //cout<<"negtive"<<endl;
- 
-   //   }
-    // }
-    //
-         fout33<<"nngp:"<<i<<": ("<<nngp[i].cordx<<","<<nngp[i].cordy<<"):"<<"distance"<<nngp[i].nngDist<<endl;
-         
-
-    }
- fout33.close();
-*/
-
-
-
-
-
-
-
-//int minVal=0;
-//
-//
-//
-//
-//
-/*
-for(int i = 0; i < id; i++) {
-    for(int j = 0; j < id; j++) {
-     //fout32<<neighDis(i,j)<<",";
-     if(neighDis(i,j)<0){
-     cout<<"negtive"<<endl;
-
-      }
-     }
-}
-*/
- /*
-struct nngNode nngGraph[ragNode];
-int index=0;
-
-
-#pragma omp parallel for
-for(int i=0;i<id;i++){
- nngNode minVal=nngNode();
- for(int j=0;j<id;j++){
-  //  int minVal=0;
-  if(neighDis(i,j)>=0){
-    if(minVal.nngDist==0){
-       minVal.nngDist=neighDis(i,j);
-       minVal.cordx=i;
-       minVal.cordy=j;
-       minVal.pixelnngVal=pixelSum[i];
-       minVal.pixelnngNum=pixelNum[i];
-      }
-      else {
-         if(minVal.nngDist>=neighDis(i,j)){
-            minVal.nngDist=neighDis(i,j);
-            minVal.cordx=i;
-            minVal.cordy=j;
-            minVal.pixelnngVal=pixelSum[i];
-            minVal.pixelnngNum=pixelNum[i];
-          }  
-
-      }
-    }
-  }      
-       if(minVal.nngDist>0){
-          nngGraph[index].nngDist = minVal.nngDist;
-          nngGraph[index].cordx = minVal.cordx;
-          nngGraph[index].cordy = minVal.cordy;
-          nngGraph[index].pixelnngVal = minVal.pixelnngVal;
-          nngGraph[index].pixelnngNum = minVal.pixelnngNum;
-          index++;
-	  cout<<"over 0"<<endl;
-
-          }
-         if(minVal.nngDist==0){
-           nngGraph[index].nngDist = minVal.nngDist;
-           nngGraph[index].cordx = minVal.cordx;
-           nngGraph[index].cordy = minVal.cordy;
-           nngGraph[index].pixelnngVal = minVal.pixelnngVal;
-           nngGraph[index].pixelnngNum = minVal.pixelnngNum;
-	   index++;
-	 }
-}
-
-//struct nngNode nngGraph[rage]
-
-cout<<nngGraph[3].nngDist<<endl;
-cout<<nngGraph[233].nngDist<<endl;
-cout<<nngGraph[500].nngDist<<endl;
-cout<<nngGraph[335].nngDist<<endl;
-cout<<nngGraph[78].nngDist<<endl;
-*/
-//int nngrows=ragNode/2;
-//cout<<"RAG nodes:"<<ragNode/2<<endl;
-
-
-//Array2D<double>nngGraph(nngrows, nngrows,0.0);
        cv::addWeighted(duplImage,0.1,testDuplicate,0.9,0,dstImage);
 
     
